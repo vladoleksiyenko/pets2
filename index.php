d6bbaa1b8bbe7d06bb6d3a1345e463f27825dcba
Vlad Oleksiyenko <vladolek2003@gmail.com>
2024-04-18 13:52:34 -0400

    Creates ht acces file and added images directory, as well as home.html page

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php
+    // 328/pets2/index.php
 
-// 328/pets2/index.php
+    // Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
 
-// Turn on error reporting
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+    // Require the autoload file
+    require_once ('vendor/autoload.php');
 
-// Require the autoload file
-require_once ('vendor/autoload.php');
+    // Instantiate the F3 Base class
+    $f3 = Base::instance();
 
-// Instantiate the F3 Base class
-$f3 = Base::instance();
+    // Define a default route
+    // https://voleksiyenko.greenriverdev.com/328/pets2
+    $f3->route('GET /', function() {
+        $view = new Template();
+        echo $view->render('views/home-page.html');
+    });
 
-// Define a default route
-// https://voleksiyenko.greenriverdev.com/328/pets2
-$f3->route('GET /', function() {
-    echo '<h1>Hello from My Diner App!</h1>';
-
-    // Render a view page
-//    $view = new Template();
-//    echo $view->render('views/home-page.html');
-});
+    $f3 -> run();
+?>
